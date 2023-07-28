f37af29ecd51c5c7d270d2d2e3e979ed958e5484
Rioba Ian <riobafelix@gmail.com>
2023-07-28 17:12:23 +0300

    create navbar and styling of navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Magnify from "svelte-material-icons/Magnify.svelte";
+  let windBnbLogo = "/logo.svg";
+</script>
+
+<nav class="py-6">
+  <div class="navbar flex items-center justify-between">
+    <img src={windBnbLogo} width="120" height="100" alt="windbnb logo" />
+
+    <form id="search-results" class="flex items-center">
+      <input
+        placeholder="Helsinki, Finland"
+        class="shadow appearance-none border rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type="text"
+        name="location"
+        id="location"
+      />
+      <input
+        placeholder="Add guests"
+        class="shadow appearance-none border rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type="text"
+        name="location"
+        id="location"
+      />
+      <label for="location" class="py-2 px-3 outline-4 cursor-pointer"
+        ><Magnify size={32} color="red" /></label
+      >
+    </form>
+  </div>
+</nav>
+
+<style>
+  #search-results {
+    /* outline: 2px solid pink; */
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Navbar from "$lib/Navbar.svelte";
+</script>
+
+<div id="app-container">
+  <Navbar />
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</div>
+
+<style>
+  #app-container {
+    width: min(80%, 1440px);
+    margin: 0 auto;
+  }
+</style>
