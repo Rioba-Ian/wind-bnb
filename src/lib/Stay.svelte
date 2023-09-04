3181444c0b99cdcc2fefb07276d6a7f3ac011c2a
Rioba Ian <riobafelix@gmail.com>
2023-09-04 21:23:55 +0300

    styling of stays

--- src/lib/Stay.svelte
+++ src/lib/Stay.svelte
@@ -1,1 +1,28 @@
-This is a single stay
+<script lang="ts">
+  interface Stay {
+    city: string;
+    country: string;
+    superHost: boolean;
+    title: string;
+    rating: number;
+    maxGuests: number;
+    type: string;
+    beds: number | null;
+    photo: string;
+  }
+  export let stay: Stay;
+</script>
+
+<article>
+  <img src={stay.photo} alt="stay.title" />
+
+  <div>
+    {#if stay.superHost}
+      <span>Super Host</span>
+    {/if}
+    <p>{stay.type} . {stay.beds} beds</p>
+    <p>{stay.rating}</p>
+  </div>
+
+  <h2>{stay.title}</h2>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
   import Navbar from "$lib/Navbar.svelte";
   import Stay from "$lib/Stay.svelte";
+  import staysData from "../stays.json";
+
+  export let stays = staysData;
 </script>
 
 <div id="app-container">
@@ -8,10 +11,19 @@
   <main class="py-8">
     <div id="title" class="flex items-center justify-between">
       <h1 class="text-neutral-800 text-3xl font-bold">Stays in Finland</h1>
-      <p>+stays</p>
+      <p class="text-zinc-800">{stays.length}+stays</p>
     </div>
     <div id="content" class="py-5">
-      <Stay />
+      <section
+        id="stays-grid"
+        class="grid grid-col-1 grid-cols-3 gap-x-8 gapy-6 grid-flow-row-dense"
+      >
+        {#each stays as stay}
+          <div class="py-5">
+            <Stay {stay} />
+          </div>
+        {/each}
+      </section>
     </div>
   </main>
 </div>
@@ -20,5 +32,29 @@
   #app-container {
     width: min(80%, 1440px);
     margin: 0 auto;
+  }
+
+  #stays-grid {
+    grid-auto-rows: auto;
+  }
+
+  #stays-grid > div:nth-child(1) {
+    grid-column: span 1;
+  }
+
+  #stays-grid > div:nth-child(2) {
+    grid-column: span 2;
+  }
+
+  #stays-grid > div:nth-child(2) {
+    grid-row: span 2;
+  }
+
+  #stays-grid > div:nth-child(4) {
+    grid-row: span 2;
+  }
+
+  #stays-grid > div:nth-child(10) {
+    grid-column: 1/ 2;
   }
 </style>
