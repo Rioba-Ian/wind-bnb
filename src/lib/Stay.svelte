1d83ed11fbda3df5d5a69d63abe2dfc3bf0457c4
Rioba Ian <riobafelix@gmail.com>
2023-09-15 11:18:20 +0300

    iconify adding svelte

--- src/lib/Stay.svelte
+++ src/lib/Stay.svelte
@@ -11,18 +11,35 @@
     photo: string;
   }
   export let stay: Stay;
+
+  import "iconify-icon";
 </script>
 
 <article>
   <img src={stay.photo} alt="stay.title" />
 
-  <div>
+  <div class="my-4 flex justify-between">
     {#if stay.superHost}
-      <span>Super Host</span>
+      <span class="border-2 border-spacing-6 px-2 border-gray-500 rounded-md"
+        >Super Host</span
+      >
     {/if}
-    <p>{stay.type} . {stay.beds} beds</p>
-    <p>{stay.rating}</p>
+    <p class="text-gray-400 font-medium ml-auto">
+      {stay.type}
+      {#if stay.beds}
+        <span>.</span>
+        <span>{stay.beds} beds</span>
+      {/if}
+    </p>
+    <p class="ml-4">
+      <iconify-icon
+        class="iconify inline-block bg-red-500"
+        data-icon="mdi:star"
+        data-inline="false"
+      />
+      {stay.rating}
+    </p>
   </div>
 
-  <h2>{stay.title}</h2>
+  <h2 class="font-bold text-gray-600">{stay.title}</h2>
 </article>
