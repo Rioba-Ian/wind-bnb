952ca3f2a7f86df2181f260d3e61eb8f7361ded2
Rioba Ian <riobafelix@gmail.com>
2023-09-15 12:28:12 +0300

    styling of text inside stays

--- src/lib/Stay.svelte
+++ src/lib/Stay.svelte
@@ -12,34 +12,36 @@
   }
   export let stay: Stay;
 
-  import "iconify-icon";
+  import Icon from "@iconify/svelte";
 </script>
 
 <article>
   <img src={stay.photo} alt="stay.title" />
 
-  <div class="my-4 flex justify-between">
+  <div class="my-2 flex items-center justify-start">
     {#if stay.superHost}
-      <span class="border-2 border-spacing-6 px-2 border-gray-500 rounded-md"
-        >Super Host</span
-      >
+      <p class="mr-8">
+        <span
+          class="border-2 border-spacing-6 px-3 py-1 border-gray-500 rounded-xl"
+          >Super Host</span
+        >
+      </p>
     {/if}
-    <p class="text-gray-400 font-medium ml-auto">
-      {stay.type}
-      {#if stay.beds}
-        <span>.</span>
-        <span>{stay.beds} beds</span>
-      {/if}
-    </p>
-    <p class="ml-4">
-      <iconify-icon
-        class="iconify inline-block bg-red-500"
-        data-icon="mdi:star"
-        data-inline="false"
-      />
-      {stay.rating}
-    </p>
+    <div class="flex items-center justify-between flex-grow" id="house-ratings">
+      <p class="text-gray-400 flex items-center">
+        <span>{stay.type}</span>
+        {#if stay.beds}
+          <span class="relative bottom-[0.25em] px-2 text-2xl font-bold">.</span
+          >
+          <span>{stay.beds} beds</span>
+        {/if}
+      </p>
+      <p class="flex items-center ml-auto">
+        <Icon icon="clarity:favorite-solid" color="#EB5757" class="mr-2" />
+        {stay.rating}
+      </p>
+    </div>
   </div>
 
-  <h2 class="font-bold text-gray-600">{stay.title}</h2>
+  <h2 class="font-bold text-neutral-800">{stay.title}</h2>
 </article>
